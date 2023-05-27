e888651f873df1cca2cb48636ada2ff9035093a3
fahzy <kabeloleiee@gmail.com>
2023-05-27 22:25:59 +0200

    Fixed register test unit test

--- backend/test/app.test.js
+++ backend/test/app.test.js
@@ -3,27 +3,49 @@ const expect = chai.expect;
 const request = require('supertest');
 const app = require('../src/app');
 const {User, Media} = require("../src/schemas");
+const configs = require('./config')
+const AWS = require("aws-sdk");
 
-describe('User Registration', () => {
-    beforeEach(async () => {
+const REGION = configs.AWS.region
+const ACCESS_KEY = configs.AWS.accessKeyId
+const SECRET_KEY = configs.AWS.secretAccessKey
+
+AWS.config.update({
+    region: REGION,
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_KEY
+});
+
+// Create an IAM instance
+const iam = new AWS.IAM();
+describe('User Registration', function()  {
+    this.timeout(10000)
+    before(async function()  {
         // Before each test, reset the database or perform any necessary setup
+        const users = await getUsersByGroup("wmpUsers")
+        for (const user of users){
+            await deleteIAMUser(user)
+        }
         await User.deleteMany();
-        await Media.deleteMany()
+        await Media.deleteMany();
         // Add test data if needed
     });
 
-    it('should register a new user', async(done) => {
-        const res = await request(app)
+    it('should register a new user', (done) => {
+        request(app)
             .post('/register')
+            .set('Content-Type', 'application/x-www-form-urlencoded')
             .send({
                 username: 'testuser',
                 password: 'testpassword',
                 role: 'wmpUsers'
             })
-            .expect(200);
+            .expect(200)
+            .end((err, res)=>{
+                expect(res.body).to.have.property('token');
+                done();
+            })
 
-        expect(res.body).to.have.property('token');
-        done();
     });
 
     // Add more test cases for edge cases and error scenarios
@@ -190,3 +212,57 @@ describe('File Deletion', () => {
 
     // Add more test cases for edge cases and error scenarios
 });
+
+// Helper functions
+const deleteIAMUser = async (userName) => {
+    try {
+        const params = {
+            UserName: userName,
+        };
+        await removeUserFromGroups((userName));
+        await iam.deleteUser(params).promise();
+
+        console.log(`IAM user '${userName}' deleted successfully.`);
+    } catch (err) {
+        console.error('Error deleting IAM user:', err);
+    }
+};
+
+async function getUsersByGroup(groupName) {
+    try {
+        const groupResponse = await iam.getGroup({ GroupName: groupName }).promise();
+
+        const userList = [];
+
+        for (const user of groupResponse.Users) {
+            const userDetails = await iam.listGroupsForUser({ UserName: user.UserName }).promise();
+            const groups = userDetails.Groups.map(group => group.GroupName);
+            userList.push( user.UserName);
+        }
+
+        return userList;
+
+    } catch (err) {
+        console.error('Error retrieving users:', err);
+    }
+}
+
+async function removeUserFromGroups(username) {
+    try {
+        const groups = await iam.listGroupsForUser({ UserName: username }).promise();
+        const groupNames = groups.Groups.map(group => group.GroupName);
+
+        // Remove user from each group
+        await Promise.all(
+            groupNames.map(groupName => {
+                return iam.removeUserFromGroup({ GroupName: groupName, UserName: username }).promise();
+            })
+        );
+
+        console.log('User removed from groups successfully.');
+    } catch (error) {
+        console.error('Error removing user from groups:', error);
+        throw error;
+    }
+}
+
