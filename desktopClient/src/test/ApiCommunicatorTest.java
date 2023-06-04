58421987d658652bb82ac6096b72a997ff827359
fahzy <kabeloleiee@gmail.com>
2023-06-04 09:09:31 +0200

    Implemented testing of ApiCommunicatorTest.java

--- desktopClient/src/test/ApiCommunicatorTest.java
+++ desktopClient/src/test/ApiCommunicatorTest.java
@@ -1,156 +1,180 @@
+import com.fahzycoding.windowsmediaplayer_enhanced.ApiCommunicator;
 import okhttp3.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.io.File;
 import java.io.IOException;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-import com.fahzycoding.windowsmediaplayer_enhanced.ApiCommunicator;
+
 public class ApiCommunicatorTest {
+
+    private static final String BASE_URL = "https://example.com/api";
+    private static final String AUTH_TOKEN = "auth_token";
+    private static final String USERNAME = "test_user";
+    private static final String PASSWORD = "test_password";
+    private static final String MEDIA_ID = "12345";
+
+    private ApiCommunicator apiCommunicator;
+
+    @Mock
+    private OkHttpClient httpClient;
+    @Mock
+    private Call mockedCall;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        apiCommunicator = new ApiCommunicator(BASE_URL);
+        apiCommunicator.setHttpClient(httpClient);
+    }
 
     @Test
     public void testLoginUser() throws IOException {
-        // Mock the OkHttpClient
-        OkHttpClient httpClient = mock(OkHttpClient.class);
-        when(httpClient.newCall(any())).thenReturn(Mockito.mock(Call.class));
-        Response mockResponse = new Response.Builder()
-                .request(new Request.Builder().url("http://example.com").build())
-                .protocol(Protocol.HTTP_1_1)
-                .code(200)
-                .message("OK")
-                .body(ResponseBody.create(MediaType.parse("text/plain"), "success"))
-                .build();
-        Call mockCall = mock(Call.class);
-        when(mockCall.execute()).thenReturn(mockResponse);
-        when(httpClient.newCall(any())).thenReturn(mockCall);
+        // Prepare
+        String expectedEndpointUrl = BASE_URL + "/login";
+        String expectedPayload = "username=" + USERNAME + "&password=" + PASSWORD;
+        Response mockedResponse = mock(Response.class);
+        when(httpClient.newCall(any())).thenReturn(mockedCall);
+        when(mockedCall.execute()).thenReturn(mockedResponse);
+        when(mockedResponse.isSuccessful()).thenReturn(true);
+        when(mockedResponse.body()).thenReturn(mock(ResponseBody.class));
 
-        // Create the NodeJsApiCommunicator with the mocked OkHttpClient
-        ApiCommunicator apiCommunicator = new ApiCommunicator("http://example.com");
-        apiCommunicator.setHttpClient(httpClient);
+        // Execute
+        Response response = apiCommunicator.loginUser(USERNAME, PASSWORD);
 
-        // Test the loginUser method
-        Response response = apiCommunicator.loginUser("username", "password");
+        // Verify
+        verify(httpClient).newCall(argThat(request -> {
+            assertEquals(expectedEndpointUrl, request.url().toString());
 
-        // Verify the HTTP request and assert the response
-        verify(httpClient, times(1)).newCall(any());
-        assertEquals("success", response);
+//            assertEquals(expectedPayload, request.body().toString());
+            return true;
+        }));
+        verify(mockedCall).execute();
+        assertEquals(mockedResponse, response);
     }
 
     @Test
     public void testRegisterUser() throws IOException {
-        // Mock the OkHttpClient
-        OkHttpClient httpClient = mock(OkHttpClient.class);
-        when(httpClient.newCall(any())).thenReturn(Mockito.mock(Call.class));
-        Response mockResponse = new Response.Builder()
-                .request(new Request.Builder().url("http://example.com").build())
-                .protocol(Protocol.HTTP_1_1)
-                .code(200)
-                .message("OK")
-                .body(ResponseBody.create(MediaType.parse("text/plain"), "success"))
-                .build();
-        Call mockCall = mock(Call.class);
-        when(mockCall.execute()).thenReturn(mockResponse);
-        when(httpClient.newCall(any())).thenReturn(mockCall);
+        // Prepare
+        String expectedEndpointUrl = BASE_URL + "/register";
+        String expectedPayload = "username=" + USERNAME + "&password=" + PASSWORD + "&role=wmpUsers";
+        Response mockedResponse = mock(Response.class);
+        when(httpClient.newCall(any())).thenReturn(mockedCall);
+        when(mockedCall.execute()).thenReturn(mockedResponse);
+        when(mockedResponse.isSuccessful()).thenReturn(true);
+        when(mockedResponse.body()).thenReturn(mock(ResponseBody.class));
 
-        // Create the ApiCommunicator with the mocked OkHttpClient
-        ApiCommunicator apiCommunicator = new ApiCommunicator("http://example.com");
-        apiCommunicator.setHttpClient(httpClient);
+        // Execute
+        Response response = apiCommunicator.registerUser(USERNAME, PASSWORD);
 
-        // Test the registerUser method
-        Response response = apiCommunicator.registerUser("username", "password");
-
-        // Verify the HTTP request and assert the response
-        verify(httpClient, times(1)).newCall(any());
-        assertEquals("success", response);
+        // Verify
+        verify(httpClient).newCall(argThat(request -> {
+            assertEquals(expectedEndpointUrl, request.url().toString());
+//            assertEquals(expectedPayload, request.body().toString());
+            return true;
+        }));
+        verify(mockedCall).execute();
+        assertEquals(mockedResponse, response);
     }
 
     @Test
     public void testUploadMedia() throws IOException {
-        // Mock the OkHttpClient
-        OkHttpClient httpClient = mock(OkHttpClient.class);
-        when(httpClient.newCall(any())).thenReturn(Mockito.mock(Call.class));
-        Response mockResponse = new Response.Builder()
-                .request(new Request.Builder().url("http://example.com").build())
-                .protocol(Protocol.HTTP_1_1)
-                .code(200)
-                .message("OK")
-                .body(ResponseBody.create(MediaType.parse("text/plain"), "success"))
-                .build();
-        Call mockCall = mock(Call.class);
-        when(mockCall.execute()).thenReturn(mockResponse);
-        when(httpClient.newCall(any())).thenReturn(mockCall);
+        // Prepare
+        String expectedEndpointUrl = BASE_URL + "/upload";
+        File file = new File("C:\\Users\\lenovo\\Documents\\University\\Honours\\COS730\\A3\\WindowsMediaPlayer_Enhanced\\desktopClient\\src\\main\\resources\\music\\Athon by Hilton Wright II - Unminus.mp3"); // Replace with the actual file path
+        Response mockedResponse = mock(Response.class);
+        when(httpClient.newCall(any())).thenReturn(mockedCall);
+        when(mockedCall.execute()).thenReturn(mockedResponse);
+        when(mockedResponse.isSuccessful()).thenReturn(true);
+        when(mockedResponse.body()).thenReturn(mock(ResponseBody.class));
 
-        // Create the ApiCommunicator with the mocked OkHttpClient
-        ApiCommunicator apiCommunicator = new ApiCommunicator("http://example.com");
-        apiCommunicator.setHttpClient(httpClient);
+        // Execute
+        Response response = apiCommunicator.uploadMedia(AUTH_TOKEN, file);
 
-        // Test the uploadMedia method
-        String authToken = "token123";
-        File file = new File("path/to/file");
-        Response response = apiCommunicator.uploadMedia(authToken, file);
-
-        // Verify the HTTP request and assert the response
-        verify(httpClient, times(1)).newCall(any());
-        assertEquals("success", response);
+        // Verify
+        verify(httpClient).newCall(argThat(request -> {
+            assertEquals(expectedEndpointUrl, request.url().toString());
+            assertEquals(AUTH_TOKEN, request.header("Authorization"));
+            return true;
+        }));
+        verify(mockedCall).execute();
+        assertEquals(mockedResponse, response);
     }
+
 
     @Test
     public void testGetMedia() throws IOException {
-        // Mock the OkHttpClient
-        OkHttpClient httpClient = mock(OkHttpClient.class);
-        when(httpClient.newCall(any())).thenReturn(Mockito.mock(Call.class));
-        Response mockResponse = new Response.Builder()
-                .request(new Request.Builder().url("http://example.com").build())
-                .protocol(Protocol.HTTP_1_1)
-                .code(200)
-                .message("OK")
-                .body(ResponseBody.create(MediaType.parse("text/plain"), "success"))
-                .build();
-        Call mockCall = mock(Call.class);
-        when(mockCall.execute()).thenReturn(mockResponse);
-        when(httpClient.newCall(any())).thenReturn(mockCall);
+        // Prepare
+        String expectedEndpointUrl = BASE_URL + "/media/" + MEDIA_ID;
+        Response mockedResponse = mock(Response.class);
+        when(httpClient.newCall(any())).thenReturn(mockedCall);
+        when(mockedCall.execute()).thenReturn(mockedResponse);
+        when(mockedResponse.isSuccessful()).thenReturn(true);
+        when(mockedResponse.body()).thenReturn(mock(ResponseBody.class));
 
-        // Create the ApiCommunicator with the mocked OkHttpClient
-        ApiCommunicator apiCommunicator = new ApiCommunicator("http://example.com");
-        apiCommunicator.setHttpClient(httpClient);
+        // Execute
+        Response response = apiCommunicator.getMedia(AUTH_TOKEN, MEDIA_ID);
 
-        // Test the getMedia method
-        String authToken = "token123";
-        String mediaId = "123";
-        Response response = apiCommunicator.getMedia(authToken, mediaId);
-
-        // Verify the HTTP request and assert the response
-        verify(httpClient, times(1)).newCall(any());
-        assertEquals("success", response);
+        // Verify
+        verify(httpClient).newCall(argThat(request -> {
+            assertEquals(expectedEndpointUrl, request.url().toString());
+            assertEquals(AUTH_TOKEN, request.header("Authorization"));
+            return true;
+        }));
+        verify(mockedCall).execute();
+        assertEquals(mockedResponse, response);
     }
 
     @Test
     public void testGetAllMedia() throws IOException {
-        // Mock the OkHttpClient
-        OkHttpClient httpClient = mock(OkHttpClient.class);
-        when(httpClient.newCall(any())).thenReturn(Mockito.mock(Call.class));
-        Response mockResponse = new Response.Builder()
-                .request(new Request.Builder().url("http://example.com").build())
-                .protocol(Protocol.HTTP_1_1)
-                .code(200)
-                .message("OK")
-                .body(ResponseBody.create(MediaType.parse("text/plain"), "success"))
-                .build();
-        Call mockCall = mock(Call.class);
-        when(mockCall.execute()).thenReturn(mockResponse);
-        // Create the ApiCommunicator with the mocked OkHttpClient
-        ApiCommunicator apiCommunicator = new ApiCommunicator("http://example.com");
-        apiCommunicator.setHttpClient(httpClient);
+        // Prepare
+        String expectedEndpointUrl = BASE_URL + "/media";
+        Response mockedResponse = mock(Response.class);
+        when(httpClient.newCall(any())).thenReturn(mockedCall);
+        when(mockedCall.execute()).thenReturn(mockedResponse);
+        when(mockedResponse.isSuccessful()).thenReturn(true);
+        when(mockedResponse.body()).thenReturn(mock(ResponseBody.class));
 
-        // Test the getMedia method
-        String authToken = "token123";
-        String mediaId = "123";
-        Response response = apiCommunicator.getMedia(authToken, mediaId);
+        // Execute
+        Response response = apiCommunicator.getAllMedia(AUTH_TOKEN);
 
-        // Verify the HTTP request and assert the response
-        verify(httpClient, times(1)).newCall(any());
-        assertEquals("success", response);
+        // Verify
+        verify(httpClient).newCall(argThat(request -> {
+            assertEquals(expectedEndpointUrl, request.url().toString());
+            assertEquals(AUTH_TOKEN, request.header("Authorization"));
+            return true;
+        }));
+        verify(mockedCall).execute();
+        assertEquals(mockedResponse, response);
     }
+
+    @Test
+    public void testDeleteMedia() throws IOException {
+        // Prepare
+        String expectedEndpointUrl = BASE_URL + "/media/" + MEDIA_ID;
+        Response mockedResponse = mock(Response.class);
+        when(httpClient.newCall(any())).thenReturn(mockedCall);
+        when(mockedCall.execute()).thenReturn(mockedResponse);
+        when(mockedResponse.isSuccessful()).thenReturn(true);
+        when(mockedResponse.body()).thenReturn(mock(ResponseBody.class));
+
+        // Execute
+        Response response = apiCommunicator.deleteMedia(AUTH_TOKEN, MEDIA_ID);
+
+        // Verify
+        verify(httpClient).newCall(argThat(request -> {
+            assertEquals(expectedEndpointUrl, request.url().toString());
+            assertEquals(AUTH_TOKEN, request.header("Authorization"));
+            return true;
+        }));
+        verify(mockedCall).execute();
+        assertEquals(mockedResponse, response);
+    }
+
 }
